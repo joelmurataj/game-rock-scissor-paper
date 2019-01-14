5ea64259bcf0fbf23afb85f681dfdc9f933bcccd
user <user@DESKTOP-G44DGFH.ads.dlh.de>
2019-01-14 07:37:25 -0800

    refactor

--- game/src/game/Player.java
+++ game/src/game/Player.java
@@ -5,21 +5,12 @@ public class Player {
 	private String firstName;
 	private String lastName;
 	private int age;
-	private Type type;
 	private int score;
 	
 	public Player(String firstName, String lastName, int age) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
-		score=0;
-	}
-	
-	public Player(String firstName, String lastName, int age, Type type) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.type = type;
 		score=0;
 	}
 	
@@ -50,15 +41,6 @@ public class Player {
 	}
 	public int getScore() {
 		return score;
-	}
-	public Type getType() {
-		return type;
-	}
-	public void setType(Type type) {
-		this.type = type;
-	}
-	public void setScore(int score) {
-		this.score = score;
 	}
 	
 }
